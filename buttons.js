f0568e6784d7484305d82030f0a625bc40ff5452
Damian Czernikowski <damianczern@gmail.com>
2018-11-06 13:16:53 -0500

    Three buttons are completed, just one more left to complete

---
+++ buttons.js
@@ -0,0 +1,16 @@
+function helloConsole () {
+  console.log("Hello, World!");
+}
+
+function helloAlert () {
+  alert("Hello, World!");
+}
+
+function helloBrowser () {
+
+}
+
+function helloStranger () {
+  var variable = prompt("Enter Your Name");
+  console.log("Hello, " + variable + "!");
+}
